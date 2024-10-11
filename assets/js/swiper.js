7b5c54d6c72ccec1660ecd6f1e4652ed8200bcc5
Sherry Wu <shw18731@gmail.com>
2024-10-11 19:29:05 +0800

    merge all pages

--- assets/js/swiper.js
+++ assets/js/swiper.js
@@ -1,1 +1,18 @@
-var swiper = new Swiper(".indexItem", {});
+const swiper1 = new Swiper(".indexSwiperItem", {
+    slidesPerView: 1.2,
+    spaceBetween: 24,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+        996: {
+          slidesPerView: 4,/*(一次呈現幾張)*/
+          spaceBetween: 24 ,
+        },
+      },
+  });
