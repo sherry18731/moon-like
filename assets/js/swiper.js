3f5299efec61b30735b68b27360d0c737370b92a
Sherry Wu <shw18731@gmail.com>
2024-10-14 16:04:45 +0800

    complete index

--- assets/js/swiper.js
+++ assets/js/swiper.js
@@ -1,18 +1,34 @@
 const swiper1 = new Swiper(".indexSwiperItem", {
     slidesPerView: 1.2,
     spaceBetween: 24,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
     breakpoints: {
+      768: {
+        slidesPerView: 2,
+        grid: {
+          rows: 2,
+          fill: "row"
+        }
+      },
         996: {
           slidesPerView: 4,/*(一次呈現幾張)*/
           spaceBetween: 24 ,
         },
+      },
+  });
+
+  // comment
+  const swiper = new Swiper(".commentSwiper", {
+    slidesPerView: 1,
+    spaceBetween: 12,
+    pagination: {
+      el: ".swiper-pagination",
+    },
+    breakpoints: {
+      768: {
+        slidesPerView: 2,
+      },
+      996: {
+        slidesPerView: 3,
+      },
       },
   });
